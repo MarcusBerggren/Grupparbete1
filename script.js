1e087d81f09b4e4956871029cf095b61a3eb5b86
Tixalore92 <n.larsson@lexpa26.onmicrosoft.com>
2026-08-18 11:18:53 +0200

    Changes in script

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log("JavaScript Hoppas jag fungerar!");
+console.log("Här är mina grejjer!");
 const button = document.querySelector("#myButton"); 
 button.addEventListener("click", () => { 
 coonsole.log("Buttons clicked!"); 
